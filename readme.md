1284c6da8660e203d4144f1e0f566c5c1f70f6ca
LeoM10-Git <cc20082012@yahoo.com.sg>
2022-01-05 17:19:51 +0800

    add checking coding

---
+++ readme.md
@@ -0,0 +1,15 @@
+WorkShop 11
+1. Create
+2. Update
+3. Delete
+4. q
+5. WorkShop 
+6. ```public class Controller {
+
+   @GetMapping("/info")
+   public String home() {
+   return "info";
+   }
+
+}
+```
--- src/main/java/ibf2021/d11/day11/Day11Application.java
+++ src/main/java/ibf2021/d11/day11/Day11Application.java
@@ -1,13 +1,30 @@
 package ibf2021.d11.day11;
 
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.DefaultApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Collections;
 
 @SpringBootApplication
 public class Day11Application {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Day11Application.class, args);
+
+        public static void main(String[] args) {
+            SpringApplication app = new SpringApplication(Day11Application.class);
+            String port = "8000";
+            ApplicationArguments cliOpts = new DefaultApplicationArguments(args);
+            if (cliOpts.containsOption("port")) {
+                port = cliOpts.getOptionValues("port").get(0); // get the first value
+            }
+            if (System.getenv("PORT")!=null){
+                port = System.getenv("PORT");
+            }
+            app.setDefaultProperties( Collections.singletonMap("server.port", port));
+            System.out.printf("Application started on port %s\n",port);
+            app.run(args);
+        }
     }
 
-}
+
